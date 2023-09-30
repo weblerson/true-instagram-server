b469b46f962950a2d9b58c40145babc437ad5f76
weblerson <zonejr4@gmail.com>
2023-09-30 14:17:19 -0400

    first application commit

---
+++ src/main/java/com/lerson/clonegram/application/ClonegramApplication.java
@@ -0,0 +1,13 @@
+package com.lerson.clonegram.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lerson/clonegram/ClonegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lerson.clonegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
