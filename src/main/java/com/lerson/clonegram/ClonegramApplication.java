9d028b6d20bca43632d604b701500debabc6e526
weblerson <zonejr4@gmail.com>
2023-10-01 13:24:20 -0400

    fix: moved main Application class to root

--- src/main/java/com/lerson/clonegram/ClonegramApplication.java
+++ src/main/java/com/lerson/clonegram/ClonegramApplication.java
@@ -1,4 +1,4 @@
-package com.lerson.clonegram.application;
+package com.lerson.clonegram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
