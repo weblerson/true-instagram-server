c63082f862fd9bc6025c49e6bd113025e87c1674
weblerson <zonejr4@gmail.com>
2023-10-01 15:45:14 -0400

    fix: removed unused import

--- src/main/java/com/lerson/clonegram/interfaces/MultipartFileHandlerInterface.java
+++ src/main/java/com/lerson/clonegram/interfaces/MultipartFileHandlerInterface.java
@@ -1,6 +1,5 @@
 package com.lerson.clonegram.interfaces;
 
-import com.lerson.clonegram.services.MultipartFileHandler;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
