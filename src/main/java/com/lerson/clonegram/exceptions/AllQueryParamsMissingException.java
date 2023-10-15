787b2e4d97e942efe68660ae0ddad5c19771993e
weblerson <zonejr4@gmail.com>
2023-10-15 16:54:22 -0400

    feat: created AllQueryParamsMissingException exception

---
+++ src/main/java/com/lerson/clonegram/exceptions/AllQueryParamsMissingException.java
@@ -0,0 +1,9 @@
+package com.lerson.clonegram.exceptions;
+
+public class AllQueryParamsMissingException extends RuntimeException {
+
+    public AllQueryParamsMissingException(String msg) {
+
+        super(msg);
+    }
+}
