3dbcf546799f50aa45c812305504165f7b46ba91
weblerson <zonejr4@gmail.com>
2023-10-15 15:10:23 -0400

    feat: <StoryService> handled exceptions

--- src/main/java/com/lerson/clonegram/services/StoryService.java
+++ src/main/java/com/lerson/clonegram/services/StoryService.java
@@ -2,6 +2,7 @@ package com.lerson.clonegram.services;
 
 import com.lerson.clonegram.dto.StoryDTO;
 import com.lerson.clonegram.entities.StoryEntity;
+import com.lerson.clonegram.exceptions.DBException;
 import com.lerson.clonegram.models.Story;
 import com.lerson.clonegram.repository.StoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,19 +28,28 @@ public class StoryService {
         this.storageService = storageService;
     }
 
-    public StoryDTO createStory(StoryEntity storyEntity) throws IOException {
-        String fileName = this.storageService.saveFile(storyEntity.getUserAvatar(), uploadDir);
-        String imageUrl = String.format("/media/stories/%s", fileName);
+    public StoryDTO createStory(StoryEntity storyEntity) {
+        try {
+            String fileName = this.storageService.saveFile(storyEntity.getUserAvatar(), uploadDir);
+            String imageUrl = String.format("/media/stories/%s", fileName);
 
-        Story createdStory = storyRepository.save(new Story(storyEntity.getUserNickName(), imageUrl));
+            Story createdStory = storyRepository.save(new Story(storyEntity.getUserNickName(), imageUrl));
 
-        return new StoryDTO(createdStory);
+            return new StoryDTO(createdStory);
+        } catch (IllegalArgumentException e) {
+            throw new DBException(e.getMessage());
+        }
     }
 
     public List<StoryDTO> findAllStories() {
 
-        List<Story> stories = this.storyRepository.findAll();
+        try {
+            List<Story> stories = this.storyRepository.findAll();
 
-        return stories.stream().map(StoryDTO::new).toList();
+            return stories.stream().map(StoryDTO::new).toList();
+        } catch (IllegalArgumentException e) {
+
+            throw new DBException(e.getMessage());
+        }
     }
 }
