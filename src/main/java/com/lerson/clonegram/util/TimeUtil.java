7dd7fe9539d4afb48c96a1acc05cb523389cc58d
weblerson <zonejr4@gmail.com>
2023-10-15 15:09:05 -0400

    feat: created a TimeUtil class to handle time related methods

---
+++ src/main/java/com/lerson/clonegram/util/TimeUtil.java
@@ -0,0 +1,11 @@
+package com.lerson.clonegram.util;
+
+import org.springframework.stereotype.Component;
+
+public class TimeUtil {
+
+    public static Long getCurrentTimeMillis() {
+
+        return System.currentTimeMillis();
+    }
+}
