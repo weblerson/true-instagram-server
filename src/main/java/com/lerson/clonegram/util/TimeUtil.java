f5e6fa89101fce8112707644b1cdab1513bbc04b
weblerson <zonejr4@gmail.com>
2023-10-15 16:56:50 -0400

    feat: <TimeUtil> created string date parser method

--- src/main/java/com/lerson/clonegram/util/TimeUtil.java
+++ src/main/java/com/lerson/clonegram/util/TimeUtil.java
@@ -1,11 +1,26 @@
 package com.lerson.clonegram.util;
 
-import org.springframework.stereotype.Component;
+import com.lerson.clonegram.exceptions.IllegalDateFormat;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TimeUtil {
 
     public static Long getCurrentTimeMillis() {
 
         return System.currentTimeMillis();
+    }
+
+    public static Date parseDateOfString(String formattedDate) {
+
+        try {
+
+            return new SimpleDateFormat("dd/MM/yyyy").parse(formattedDate);
+        } catch (ParseException e) {
+
+            throw new IllegalDateFormat(e.getMessage());
+        }
     }
 }
