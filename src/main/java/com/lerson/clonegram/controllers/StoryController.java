d0b5154de759baf306e6c3f429b42369916630aa
weblerson <zonejr4@gmail.com>
2023-10-20 10:24:17 -0400

    feat: documented StoryController class

--- src/main/java/com/lerson/clonegram/controllers/StoryController.java
+++ src/main/java/com/lerson/clonegram/controllers/StoryController.java
@@ -3,8 +3,12 @@ package com.lerson.clonegram.controllers;
 import com.lerson.clonegram.dto.StoryDTO;
 import com.lerson.clonegram.entities.StoryEntity;
 import com.lerson.clonegram.services.StoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,14 +28,30 @@ public class StoryController {
         this.storyService = storyService;
     }
 
-    @PostMapping
-    public ResponseEntity<StoryDTO> createStory(@RequestParam("userAvatar") MultipartFile userAvatar,
-                                      @RequestParam("userNickName") String userNickName) throws IOException {
+    @Operation(
+            summary = "Create a story",
+            description = "Create a story by providing a nickname and an avatar image. " +
+                    "Returns the info from the created story."
+    )
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<StoryDTO> createStory(
+            @Parameter(
+                    description = "Avatar image to be uploaded",
+                    content = @Content(mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+            )
+            @RequestParam("userAvatar") MultipartFile userAvatar,
+
+            @Parameter(description = "User nickname")
+            @RequestParam("userNickName") String userNickName) throws IOException {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(this.storyService.createStory(new StoryEntity(userNickName, userAvatar)));
     }
 
+    @Operation(
+            summary = "Find all stories",
+            description = "Return a list with all created stories in."
+    )
     @GetMapping
     public ResponseEntity<List<StoryDTO>> findAllStories() {
 
