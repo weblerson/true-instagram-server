41ed6eddc1d3e3521a7da4e1d463f1490aff9e3b
weblerson <zonejr4@gmail.com>
2023-10-20 10:22:32 -0400

    feat: swagger application configuration

--- src/main/java/com/lerson/clonegram/ClonegramApplication.java
+++ src/main/java/com/lerson/clonegram/ClonegramApplication.java
@@ -1,9 +1,12 @@
 package com.lerson.clonegram;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Clonegram API", version = "1.0", description = "API do aplicativo Clonegram"))
 public class ClonegramApplication {
 
 	public static void main(String[] args) {
